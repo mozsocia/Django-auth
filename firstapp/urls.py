b39afe6ce04b1d609d32bdff2659d0bc481e64a6
Mozdalif sikder <mozsocia@gmail.com>
2023-07-06 15:47:25 +0600

    Update urls.py

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('hello/', hello_view, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
